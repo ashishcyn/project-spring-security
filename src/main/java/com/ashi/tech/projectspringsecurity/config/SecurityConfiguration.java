64d9bc43a7ccc41b22eebe1f229bbea18efc2447
Ashish <c2nashish@gmail.com>
2020-07-02 15:34:57 +0400

    06. @EnableWebSecurity, configure method overridden, sample PasswordEncoder also given

--- src/main/java/com/ashi/tech/projectspringsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/ashi/tech/projectspringsecurity/config/SecurityConfiguration.java
@@ -1,4 +1,34 @@
 package com.ashi.tech.projectspringsecurity.config;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //Set our configuration on the auth object
+        //Learning purpose: we used inMemoryAuthentication here
+        auth.inMemoryAuthentication()
+                .withUser("foo")
+                .password("foo")
+                .roles("USER")
+                .and()
+                .withUser("abc")
+                .password("abc")
+                .roles("ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+         //Learning purpose:
+        // Use the desired encode here.The NoOpPasswordEncoder wont encode.It will still be the plain text here
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
