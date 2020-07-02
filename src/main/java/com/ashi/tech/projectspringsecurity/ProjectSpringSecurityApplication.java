429604e34d84e5a3aba67bf5727b44b1810441df
Ashish <c2nashish@gmail.com>
2020-07-02 12:38:59 +0400

    01.Spring boot sample project

---
+++ src/main/java/com/ashi/tech/projectspringsecurity/ProjectSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ashi.tech.projectspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashi/tech/projectspringsecurity/ProjectSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashi.tech.projectspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
