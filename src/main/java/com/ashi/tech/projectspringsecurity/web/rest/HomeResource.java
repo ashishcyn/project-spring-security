fd4581a1a9d7a2ed10e7a2d79a0ca98266f280a1
Ashish <c2nashish@gmail.com>
2020-07-02 13:21:17 +0400

    02.Default rest end point added

---
+++ src/main/java/com/ashi/tech/projectspringsecurity/web/rest/HomeResource.java
@@ -0,0 +1,13 @@
+package com.ashi.tech.projectspringsecurity.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>Hello</h1>");
+    }
+}
