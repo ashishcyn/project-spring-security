f4b9595311269d66b9fdc972777a46174db840ed
Ashish <c2nashish@gmail.com>
2020-07-03 16:12:25 +0400

    12.Spring Security with JPA authentication: step3- connected with Postgres

--- src/main/java/com/ashi/tech/projectspringsecurity/ProjectSpringSecurityApplication.java
+++ src/main/java/com/ashi/tech/projectspringsecurity/ProjectSpringSecurityApplication.java
@@ -1,9 +1,13 @@
 package com.ashi.tech.projectspringsecurity;
 
+import com.ashi.tech.projectspringsecurity.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+//@EnableJpaRepositories(basePackages = "")
 public class ProjectSpringSecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ashi/tech/projectspringsecurity/service/MyUserDetailsService.java
+++ src/main/java/com/ashi/tech/projectspringsecurity/service/MyUserDetailsService.java
@@ -1,16 +1,27 @@
 package com.ashi.tech.projectspringsecurity.service;
 
+import com.ashi.tech.projectspringsecurity.domain.User;
+import com.ashi.tech.projectspringsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return new MyUserDetails(s);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+         Optional<User>  user = userRepository.findByUserName(userName);
+
+         user.orElseThrow(()-> new UsernameNotFoundException(("Not found"+ userName)));
+
+         return user.map(MyUserDetails::new).get();
     }
 }
