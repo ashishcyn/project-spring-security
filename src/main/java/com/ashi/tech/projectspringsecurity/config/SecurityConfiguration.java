2ab7aa03093aefdd3568eed8ada4ed171e35c6b9
Ashish <c2nashish@gmail.com>
2020-07-04 00:26:54 +0400

    13.Spring security Ldap authentication

--- src/main/java/com/ashi/tech/projectspringsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/ashi/tech/projectspringsecurity/config/SecurityConfiguration.java
@@ -1,49 +1,40 @@
 package com.ashi.tech.projectspringsecurity.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    UserDetailsService userDetailsService;
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        //For JDBC we have jdbcAuthentication Manager
-        //For LDAP we have LDAPprovider to configure
-        //For JPA there is no this kind of implementation for authentication
-        //In case of JPA we need to create the UserDetails Service
-        auth.userDetailsService(userDetailsService);
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordAttribute("userPassword");
+
     }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-         //Learning purpose:
-        // Use the desired encode here.The NoOpPasswordEncoder wont encode.It will still be the plain text here
-        return NoOpPasswordEncoder.getInstance();
-    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //most restrictive to lease restrictive order
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER","ADMIN")
-                .antMatchers("/").permitAll()
-                .and()
-                .formLogin();
+       http.authorizeRequests()
+               .anyRequest().fullyAuthenticated()
+               .and()
+               .formLogin();
 
     }
 }
