86a44b4b0f287afdc65b1fdffc9d64d6c2d32290
Ashish <c2nashish@gmail.com>
2020-07-04 19:40:23 +0400

    14.Spring security + JWT

--- src/main/java/com/ashi/tech/projectspringsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/ashi/tech/projectspringsecurity/config/SecurityConfiguration.java
@@ -1,40 +1,52 @@
 package com.ashi.tech.projectspringsecurity.config;
 
+import com.ashi.tech.projectspringsecurity.filters.JwtRequestFilter;
+import com.ashi.tech.projectspringsecurity.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .ldapAuthentication()
-                .userDnPatterns("uid={0},ou=people")
-                .groupSearchBase("ou=groups")
-                .contextSource()
-                .url("ldap://localhost:8389/dc=springframework,dc=org")
-                .and()
-                .passwordCompare()
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .passwordAttribute("userPassword");
-
+        auth.userDetailsService(myUserDetailsService);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.authorizeRequests()
-               .anyRequest().fullyAuthenticated()
+       http
+               .csrf().disable()
+               .authorizeRequests().antMatchers("/authenticate").permitAll()
+               .anyRequest().authenticated()
                .and()
-               .formLogin();
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+       http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
     }
 }
--- src/main/java/com/ashi/tech/projectspringsecurity/web/rest/HomeResource.java
+++ src/main/java/com/ashi/tech/projectspringsecurity/web/rest/HomeResource.java
@@ -1,23 +1,50 @@
 package com.ashi.tech.projectspringsecurity.web.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ashi.tech.projectspringsecurity.models.AuthenticationRequest;
+import com.ashi.tech.projectspringsecurity.models.AuthenticationResponse;
+import com.ashi.tech.projectspringsecurity.service.MyUserDetailsService;
+import com.ashi.tech.projectspringsecurity.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeResource {
 
-    @GetMapping("/")
-    public String home(){
-        return ("<h1>Hello</h1>");
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtil jwtTockenUtil;
+
+    @RequestMapping({"/hello"})
+    public String hello(){
+        return "Hello";
     }
 
-    @GetMapping("/user")
-    public String user(){
-        return("<h1>Welcome User</h1>");
-    }
-
-    @GetMapping("/admin")
-    public String admin(){
-        return("<h1>Welcome Admin</h1>");
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationTocken(@RequestBody AuthenticationRequest  authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = myUserDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtTockenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
     }
 }
